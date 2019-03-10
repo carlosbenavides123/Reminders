f7ac7e48f1cc4e74235981980e64eab789bab158
carlosbenavides123 <carlos7legend@gmail.com>
2019-03-10 12:02:53 -0700

    add firebase

--- app/app.js
+++ app/app.js
@@ -3,11 +3,25 @@ import Vue from "nativescript-vue";
 import Login from "./components/Login"
 import master from "./Master"
 import store from './store/store';
+import * as firebase from "nativescript-plugin-firebase"
 
 Vue.registerElement('BottomNavigation', () => require('nativescript-bottom-navigation').BottomNavigation);
 Vue.registerElement('BottomNavigationTab', () => require('nativescript-bottom-navigation').BottomNavigationTab);
 Vue.registerElement('CardView',() => require('nativescript-cardview').CardView);
 // Vue.use(Vuex)
+
+
+firebase.init({
+  // Optionally pass in properties for database, authentication and cloud messaging,
+  // see their respective docs.
+}).then(
+    function () {
+      console.log("firebase.init done");
+    },
+    function (error) {
+      console.log("firebase.init error: " + error);
+    }
+);
 
 new Vue({
     store,
