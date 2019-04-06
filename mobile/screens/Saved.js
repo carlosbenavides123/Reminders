21c68588efa5963e9adad69eae51384d5417b9f4
carlosbenavides123 <carlos7legend@gmail.com>
2019-04-06 11:15:54 -0700

    able to retrieve reminders in RN

--- mobile/screens/Saved.js
+++ mobile/screens/Saved.js
@@ -1,20 +1,62 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    ListView
 } from "react-native";
 
+
 class Saved extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            reminders: []
+        }
+      }
+
+    get_data_from_db = () => {
+        var config = {
+            headers: {
+                'Authorization': 'Token 67683fffab1fc8dcd9fe5c66e6fa9a410c73a1cd'
+            }
+        };
+        axios.get(`http://10.0.2.2:8000/api/reminder/reminder/`, config)
+        .then(res => {
+            this.setState({reminders: res.data})
+            console.log(this.state.reminders)
+        })
+        .catch( err => {
+            console.log(err);
+        });
+    }
+
+    componentDidMount() {
+        this.get_data_from_db()
+    }
+    renderRow(rowData) {
+        return (<Text>{rowData}</Text>);
+     }
+
     render() {
+        contents = this.state.reminders.map(function (item) {
+            return (
+              <View key={item.id}>
+                <Text>{item.id} {item.date} {item.name}</Text>
+              </View>
+            );
+         });
+
+        // if(this.state.reminders){
         return (
             <View style={styles.container}>
-                <Text>
-                    Saved
-                </Text>
+                    {contents}
             </View>
         );
+        // }
     }
 }
 export default Saved;
