87f0533c3d1e250c84bc699eec028cc43d496510
carlosbenavides123 <carlos7legend@gmail.com>
2019-04-07 21:13:11 -0700

    I guess this works

--- backend/reminder/views.py
+++ backend/reminder/views.py
@@ -13,6 +13,10 @@ from django.db.migrations.loader import MigrationLoader
 
 from pyfcm import FCMNotification
 
+import time as time_module
+from datetime import datetime
+from datetime import timedelta
+
 proxy_dict = {
           "http"  : "http://127.0.0.1",
           "https" : "http://127.0.0.1",
@@ -26,6 +30,13 @@ message_title = "Uber update"
 message_body = "Hi john, your customized news for today is ready"
 
 from fcm_django.models import FCMDevice
+
+from pytz import timezone
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+import os, time
+
+sched = BlockingScheduler()
 
 class ReminderViewSet(viewsets.GenericViewSet,
                      mixins.ListModelMixin,
@@ -42,23 +53,35 @@ class ReminderViewSet(viewsets.GenericViewSet,
 
     def perform_create(self, serializer):
         """ Create a new reminder"""
-        print("TESD")
-        print(registration_id)
-        print(message_title)
-        print(message_body)
-        result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
-        print(result)
+        serializer.save(user=self.request.user)
+        scheduler.add_job(self.job_function, 'cron', month=4, day=7, hour=21, minute=12, timezone=timezone('US/Pacific'))
 
-        # self.test_lol()
-        # return HttpResponse("123")
-        # serializer.save(user=self.request.user)
+    def job_function(self):
+        print("dasds")
 
-    # def test_lol(self):
-    #     device = FCMDevice.objects.all().first()
-    #     device.send_message("Title", "Message")
+    def perform_update(self, serializer):
+        """Create a new ingredient"""
+        serializers.save(user=self.request.user)
 
-    #     print("!$#@$")
+def tick(text):
+    print(text + '! The time is: %s' % datetime.now())
 
-    # def perform_update(self, serializer):
-    #     """Create a new ingredient"""
-    #     serializers.save(user=self.request.user)
+
+scheduler = BackgroundScheduler()
+dd = datetime.now() + timedelta(seconds=3)
+scheduler.add_job(tick, 'date',run_date=dd, args=['TICK'])
+
+dd = datetime.now() + timedelta(seconds=6)
+scheduler.add_job(tick, 'date',run_date=dd, kwargs={'text':'TOCK'})
+
+scheduler.start()
+print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+def pause():
+    scheduler.pause()
+    print("PAUSE")
+    scheduler.add_job(resume, trigger='cron', second='*/5')
+def resume():    
+    scheduler.resume()
+    print(RESUME)
+    scheduler.add_job(pause, trigger='cron', second='*/5')
